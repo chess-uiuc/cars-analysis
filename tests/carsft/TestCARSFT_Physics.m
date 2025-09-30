4aa68419cc7cd1b8b235840494278807ecc55159
Mike Campbell <mtcampbe@illinois.edu>
2025-09-30 09:38:40 -0500

    Tweak params for end-to-end test

--- tests/carsft/TestCARSFT_Physics.m
+++ tests/carsft/TestCARSFT_Physics.m
@@ -16,7 +16,7 @@ classdef TestCARSFT_Physics < matlab.unittest.TestCase
 
     methods (Test)
         function e2e_runs_and_has_structure(test)
-            wexp  = linspace(200, 4000, 100);
+            wexp  = linspace(2050, 2350, .1);
             T     = 300;  P = 1.0;  X = [1 0 0 0];
             dtp   = 0.5;  dtau3 = 0.1;  alpha = 0.05;  dwp = 0.5;
 
